8800cc2b2058f3d53b107c486a7a36bca851e3db
ERAGON9 <liorbarak99@gmail.com>
2024-08-16 18:14:54 +0300

    Updated FindMatchFeature.cs with functionaity.

--- FacebookWinFormsApp/FacebookLogic/FindMatchFeature.cs
+++ FacebookWinFormsApp/FacebookLogic/FindMatchFeature.cs
@@ -2,6 +2,7 @@
 using FacebookWrapper.ObjectModel;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,35 +12,151 @@ namespace BasicFacebookFeatures.FacebookLogic
 {
     public class FindMatchFeature
     {
+        private readonly int r_AgePreferenceMinRestriction = 18;
+        private readonly int r_AgePrefernceMaxRestriction = 100;
+
         public User UserLogin { get; set; }
+
         public User.eGender GenderPreference { get; set; }
-        public int AgePreferenceFrom { get; set; }
-        public int AgePrefernceTo { get; set; }
 
-        public FacebookObjectCollection<User> FindUserMatch()
+        private int m_AgePreferenceMin;
+        public int AgePreferenceMin 
         {
-            FacebookObjectCollection<User> filteredFriends = new FacebookObjectCollection<User>();
+            get
+            {
+                return m_AgePreferenceMin;
+            }
+            set
+            {
+                if (value >= r_AgePreferenceMinRestriction && value <= r_AgePrefernceMaxRestriction)
+                {
+                    m_AgePreferenceMin = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("AgePreferenceMin", "Age Preference Min need to be between 18 to 100");
+                }
+            }
+        }
 
+        private int m_AgePrefernceMax;
+        public int AgePrefernceMax
+        {
+            get
+            {
+                return m_AgePrefernceMax;
+            }
+            set
+            {
+                if (value >= r_AgePreferenceMinRestriction && value <= r_AgePrefernceMaxRestriction)
+                {
+                    m_AgePrefernceMax = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("AgePrefernceMax", "Age Preference Max need to be between 18 to 100");
+                }
+            }
+        }
+
+        public FindMatchFeature()
+        {
+            GenderPreference = User.eGender.female;
+            AgePreferenceMin = r_AgePreferenceMinRestriction;
+            AgePrefernceMax = r_AgePrefernceMaxRestriction; //TODO: change at the UI to 100!!!
+        }
+
+        public List<User> FindUserMatch()
+        {
+            throwExceptionIfUserLoginIsNull();
+            List<User> sortedFilteredFriends = null;
             try
             {
                 FacebookObjectCollection<User> friends = UserLogin.Friends;
+                List<User> filteredFriends = new List<User>();
                 foreach (User friend in friends)
                 {
-                    if (friend.Gender.Equals(GenderPreference))
+                    if (checkGenderPreference(friend) && checkAgePreference(friend))
                     {
-
-                        //friend.Birthday
-                        //if (friend.)
+                        filteredFriends.Add(friend);
                     }
                 }
 
+                sortedFilteredFriends = sortBySharedLikedPages(filteredFriends);
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Find Match Failed");
             }
 
-            return filteredFriends;
+            return sortedFilteredFriends;
         }
+
+        private void throwExceptionIfUserLoginIsNull()
+        {
+            if (UserLogin == null)
+            {
+                throw new ArgumentNullException("UserLogin","User login is null");
+            }
+        }
+
+        private List<User> sortBySharedLikedPages(List<User> i_FilteredFriends)
+        {
+            List<User> sortedFilteredFriends = i_FilteredFriends.OrderByDescending(friend => sharedLikedPagesCount(friend)).ToList();
+
+            return sortedFilteredFriends;
+        }
+
+        private int sharedLikedPagesCount(User i_Friend)
+        {
+            int countSharePages = 0;
+
+            foreach (Page friendPage in i_Friend.LikedPages)
+            {
+                foreach (Page userPage in UserLogin.LikedPages)
+                {
+                    if (friendPage.Id == userPage.Id)
+                    {
+                        countSharePages++;
+                        break;
+                    }
+                }
+            }
+
+            return countSharePages;
+        }
+
+        private bool checkGenderPreference(User i_Friend)
+        {
+            bool inGenderPrefernce = false;
+
+            if (i_Friend.Gender != null)
+            {
+                inGenderPrefernce = i_Friend.Gender.Equals(GenderPreference);
+            }
+
+            return inGenderPrefernce;
+        }
+
+        private bool checkAgePreference(User i_Friend)
+        {
+            bool inAgePreference = false;
+
+            if (i_Friend.Birthday != null)
+            {
+                DateTime friendBirthDate = DateTime.ParseExact(i_Friend.Birthday, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+                int friendAge = DateTime.Today.Year - friendBirthDate.Year;
+
+                if (DateTime.Today < friendBirthDate.AddYears(friendAge))
+                {
+                    friendAge--;
+                }
+
+                inAgePreference = friendAge >= AgePreferenceMin && friendAge <= AgePrefernceMax;
+            }
+
+            return inAgePreference;
+        }
+
     }
 }
